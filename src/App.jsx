48519ab8a008f78c6db909908a07727f6973a17e
Muhammad Nour <acadamiclearner@gmail.com>
2024-11-20 18:01:22 +0200

    set color

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
 const App = () => {
+  const [color, setColor] = useState('#ffffff');
+
+  const generateColor = () => {
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    setColor(`#${randomColor}`);
+  };
+
   return (
     <div>
-      <button>generate a color</button>
-      <h1>color Hex</h1>
+      <button onClick={generateColor}>Generate a Color</button>
+      <h1 style={{color: color}}>Color Hex: {color}</h1>
     </div>
   )
 }
